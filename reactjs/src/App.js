99490453032b964f03d6d583d83935e4b7403788
mehrez1401 <127346362+mehrez1401@users.noreply.github.com>
2023-05-29 21:26:06 +0200

    Update App.js

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
+{/* importation de notre style boostrap*/}
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+{/* importation de nos composants*/}
 import Formulo from './composant/Formulo';
 import Carous from './composant/Carous';
 function App() {
   return (
     <>
+    {/* integration ou appel de nos composants*/}
     <Carous/>
     <Formulo/>
+    {/* un simple affichage du titre à l'aide du taget h1 */}
     <h1>hello word</h1></>
   );
 }
