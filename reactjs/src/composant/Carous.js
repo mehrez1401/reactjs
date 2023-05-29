d0aaf89e5e55c9c3dd15376b2b7f8251cc39d30f
mehrez1401 <127346362+mehrez1401@users.noreply.github.com>
2023-05-29 21:19:48 +0200

    Update Carous.js

--- reactjs/src/composant/Carous.js
+++ reactjs/src/composant/Carous.js
@@ -1,15 +1,20 @@
 import React from 'react'
+{/*importation de nos composants boostrap */}
+
 import Carousel from "react-bootstrap/Carousel"
 import image1 from "../image/1.PNG"
 import image2 from "../image/2.PNG"
 import image3 from "../image/3.PNG"
 
+{/* function Carous */}
 
 const Carous = () => {
   return (
     <div>
       <Carousel>
       <Carousel.Item>
+    {/* insertion de notre image */}
+
         <img
           className="d-block w-100"
           src={image1}
